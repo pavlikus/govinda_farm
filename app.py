2236ab863e667c41c799afeeb9443df022582ed7
pavlik <readystores.ru@gmail.com>
2020-09-03 14:41:02 +0300

    sqlalchemy integration

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 
+from db import db_session
+
 
 app = Flask(__name__)
 
@@ -8,3 +10,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html', name='index')
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
